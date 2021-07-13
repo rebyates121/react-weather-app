ab7d9737f40d25815422b93f7c8eef0140444183
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-13 16:25:17 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Rebecca Yates and is {" "}
+        
+        <a href="https://github.com/rebyates121/react-weather-app" target="_blank">
+          open-sourced on Github
+        </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
