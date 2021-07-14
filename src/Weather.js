3612089ffe28936e37d99a9932ff6a8852d70853
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-14 18:25:31 +0100

    Added css

--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,55 @@
 import React from "react";
-import "./Weather.css"
+import "./Weather.css";
 
 export default function Weather() {
-    return (
-    	<div className="Weather">
-				<form>
-					<div className="row">
-						<div className="col-9">
-							<input type="search" placeholder="Enter a city..." className="form-control" />
-						</div>
-						<div className="col-3">
-							<input type="submit" value="Search" className="btn-primary" />
-							</div>
-					</div>
-				</form>
-        <h1>London</h1>
-        <ul>
-          <li>Tuesday 16:00</li>
-          <li>Partly Cloudy</li>
-        </ul>
+  return (
+    <div className="Weather">
+      <form>
         <div className="row">
-          <div className="col-6">
-						<img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-						alt="Partly Cloudy" />
-						23°C
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
           </div>
-					<div className="col-6">
-						<ul>
-							<li>
-								Precipitation: 5%
-							</li>
-							<li>
-								Humidity: 59%
-							</li>
-							<li>
-								Wind: 8mph
-							</li>
-						</ul>
-					</div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
         </div>
-    	</div>
-    )
+      </form>
+      <h1>London</h1>
+      <ul>
+        <li>Tuesday 16:00</li>
+        <li>Partly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly Cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">23</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 5%</li>
+            <li>Humidity: 59%</li>
+            <li>Wind: 8mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
