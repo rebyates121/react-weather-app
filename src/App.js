3a2af620273db8c047aa51597bea5620e990d34d
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-15 15:30:39 +0100

    Added API integration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import './App.css';
-import Weather from './Weather';
+import React from "react";
+import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <Weather />
+        <Weather defaultCity="London" />
         <footer>
-          This project was coded by Rebecca Yates and is {" "}
-        
-        <a href="https://github.com/rebyates121/react-weather-app" target="_blank">
-          open-sourced on Github
-        </a>
+          This project was coded by Rebecca Yates and is{" "}
+          <a
+            href="https://github.com/rebyates121/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
         </footer>
       </div>
     </div>
