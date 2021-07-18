9530dbf4c4d8c4860b0f4764f11a154817449b5f
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-18 15:38:21 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -19,7 +19,7 @@ export default function Weather(props) {
   }
   function search() {
     const apiKey = "efb771bfb7ce128cb70f69f945fe6a81";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
   function handleSubmit(event) {
