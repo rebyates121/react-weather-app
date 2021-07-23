bae4ef14ce5602bf6d2e8d1c6e6052d12d1e9913
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-23 15:40:03 +0100

    css changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <h1 className="Weather-app">Weather App</h1>
         <Weather defaultCity="London" />
         <footer>
           This project was coded by Rebecca Yates and is{" "}
